5035be68b3578186929b1d22fdad3328ade041dc
sona306 <sonasabu307@gmail.com>
2024-06-23 12:19:08 +0530

    form comp

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from './Nav'
+import axios from 'axios'
 
 const Signin = () => {
+    const[sigin,setData]=useState(
+        {
+            "email":"",
+            "password":""
+        }
+    )
+
+    const inputHandler=(event)=>{
+        setData({...sigin,[event.target.name]:event.target.value})
+    }
+
+    const readValue=()=>{
+       console.log(sigin)
+       axios.post("http://localhost:8080/signin",sigin).then(
+        (response)=>{
+            console.log(response.data)
+            if (response.data.status=="success") 
+            {
+                alert("Login successfull")
+            } else 
+            {
+                alert("unable")
+            }
+        }
+       ).catch() 
+    }
 
     return (
         <div>
@@ -16,14 +43,14 @@ const Signin = () => {
                                 <div className="row">
                                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"><br></br>
                                         <label htmlFor="" className="form-label" >Email</label>
-                                        <input type="text" className="form-control" placeholder="Enter your email" />
+                                        <input type="text" className="form-control" name='email' value={sigin.email} onChange={inputHandler}  placeholder="Enter your email" />
                                     </div>
                                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"><br></br>
                                         <label htmlFor="" className="form-label">Password</label>
-                                        <input type="password" className="form-control" placeholder="Enter your password" />
+                                        <input type="password" className="form-control" name='password' value={sigin.password} onChange={inputHandler} placeholder="Enter your password" />
                                     </div>
                                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6"><br></br>
-                                        <button className="btn btn-success">SignIn</button>
+                                        <button className="btn btn-success" onClick={readValue}>SignIn</button>
                                     </div>
                                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6"><br></br>
                                         <label htmlFor="" className="form-label">No account? Signup Now!</label>
